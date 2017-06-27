adc2226730b033b756b71fbc17f8355ee358d199
omkarsai <saiomkar.vudutha@archents.com>
2017-06-27 11:33:57 +0530

    firstcommit

---
+++ gogoos.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+
+/* runtime.GOOs == current operting system */
+func main() {
+	fmt.Print("Go runs on ")
+	fmt.Println(runtime.GOOS)
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
+}
